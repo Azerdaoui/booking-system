1ba809cd9cf47e41bda9ccde93d727fab618dd5b
Othmane AZERDAOUI <oazerdaoui@gear9.ma>
2024-09-13 17:26:04 +0100

    Employee schedules and exclusions

--- app/Models/ScheduleExclusion.php
+++ app/Models/ScheduleExclusion.php
@@ -5,17 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Schedule extends Model
+class ScheduleExclusion extends Model
 {
     use HasFactory;
 
     protected $casts = [
-        'starts_at' => 'date',
-        'ends_at' => 'date',
+        'starts_at' => 'datetime',
+        'ends_at' => 'datetime',
     ];
-
-    public function employee()
-    {
-        return $this->belongsTo(Employee::class);
-    }
 }
