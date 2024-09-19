b94d7badae42f8137f4f6da56f698f78bf87329b
Azerdaoui Othmane <othmane.azerdaoui@gmail.com>
2024-09-19 17:56:59 +0100

    ge Schedule Availabilty

---
+++ app/Bookings/ScheduleAvailability.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Bookings;
+
+use App\Models\Employee;
+use App\Models\Service;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
+use Spatie\Period\Boundaries;
+use Spatie\Period\Period;
+use Spatie\Period\PeriodCollection;
+use Spatie\Period\Precision;
+
+class ScheduleAvailability
+{
+    protected PeriodCollection $periods;
+
+    public function __construct(
+        protected Employee $employee,
+        protected Service $service,
+    ) {
+        $this->periods = new PeriodCollection();
+    }
+
+    public function forPeriod(Carbon $startsAt, Carbon $endsAt)
+    {
+        collect(CarbonPeriod::create($startsAt, $endsAt)->days())
+            ->each(function ($date) {
+                $this->addAvailabilityFromSchedule($date);
+            });
+
+        dd($this->periods);
+    }
+
+    protected function addAvailabilityFromSchedule(Carbon $date)
+    {
+        if (!$schedule = $this->employee->schedules->where('starts_at', '<=', $date)->where('ends_at', '>=', $date)->first()) {
+            return;
+        }
+
+        if (![$startsAt, $endsAt] = $schedule->getWorkingHoursFromDate($date)) {
+            return;
+        }
+
+        $this->periods = $this->periods->add(
+            Period::make(
+                $date->copy()->setTimeFromTimeString($startsAt),
+                $date->copy()->setTimeFromTimeString($endsAt)->subMinutes($this->service->duration),
+                Precision::MINUTE(),
+            )
+        );
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Bookings\ScheduleAvailability;
 use App\Http\Controllers\ProfileController;
+use App\Models\Employee;
+use App\Models\Service;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -18,10 +21,21 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/', function () {
+    $employee = Employee::find(1);
+    $service = Service::find(1);
+
+    $availability = (new ScheduleAvailability($employee, $service))
+        ->forPeriod(
+            now()->startOfDay(),
+            now()->addMonth()->endOfDay(),
+        );
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
