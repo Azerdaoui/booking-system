2f85ab8d4326bee9119684f2e5553914170a4e62
Azerdaoui Othmane <othmane.azerdaoui@gmail.com>
2025-01-24 17:45:45 +0100

    Employee and service resources

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,17 @@
+<script setup>
+    defineProps({
+        employees: Array,
+        services: Array,
+    });
+</script>
+
+<template>
+    <div>
+        {{ employees }}
+        {{ services }}
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -1,55 +1,6 @@
 <?php
 
-use App\Bookings\ScheduleAvailability;
-use App\Bookings\ServiceSlotAvailability;
-use App\Http\Controllers\ProfileController;
-use App\Models\Employee;
-use App\Models\Service;
-use Carbon\Carbon;
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Carbon::setTestNow(now()->setTimeFromTimeString('17:00:00'));
-
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/', function () {
-    $availability = (new ServiceSlotAvailability(Employee::get(), Service::find(1)))
-        ->forPeriod(now()->startOfDay(), now()->addDay()->endOfDay());
-
-    dd(
-        $availability
-    );
-
-    // dd(
-    //
-    // );
-    // $employee = Employee::find(1);
-    // $service = Service::find(1);
-
-    // $availability = (new ScheduleAvailability($employee, $service))
-    //     ->forPeriod(
-    //         now()->startOfDay(),
-    //         now()->addMonth()->endOfDay(),
-    //     );
-});
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
+Route::get('/', HomeController::class)->name('home');
