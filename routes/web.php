37fa1bfe2b5307a58a67690836414ebb8e28b5e3
Azerdaoui Othmane <othmane.azerdaoui@gmail.com>
2025-01-28 19:26:37 +0100

    pint

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', HomeController::class)->name('home');
 
 Route::get('/employees/{employee}', EmployeeServiceIndexController::class)->name('employee');
-
